08b7517999e2aea7dfa5a0606bc0b11bfc59fc2c
Eric Brian Njuguna <brianerick.njuguna@gmail.com>
2025-03-04 22:25:46 +0300

    Added and removedHTML elements using JS

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+
+document.querySelector("#main").remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Eric Brian Njuguna is the champion";
